2a5455685282eddf410a081ad69acc791868ed5f
ynion-mabeamae <mabeamaeynion@gmail.com>
2026-01-19 18:46:54 +0800

    refactor: update routing structure for improved navigation

--- servetrack-frontend/src/app/app.routes.ts
+++ servetrack-frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LandingPage } from './landing-page/landing-page';
+import { VotingPoll } from './voting-poll/voting-poll';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingPage },
+  { path: 'voting-poll', component: VotingPoll },
+  { path: '**', redirectTo: '' }
+];
