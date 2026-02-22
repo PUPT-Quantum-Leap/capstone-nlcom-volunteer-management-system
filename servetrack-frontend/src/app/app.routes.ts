2f673ecd43f5633c02b9b305de19bff2e449d126
mejares-jamesmichael <jamesmichaelmejares@gmail.com>
2026-02-22 16:54:09 +0800

    Add router link and ui for terms and privacy.

--- servetrack-frontend/src/app/app.routes.ts
+++ servetrack-frontend/src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { LandingPage } from './landing-page/landing-page';
 import { VotingPoll } from './voting-poll/voting-poll';
 import { Login } from './auth/login/login';
 import { Signup } from './auth/signup/signup';
+import { TermsOfService } from './terms-of-service/terms-of-service';
+import { PrivacyPolicy } from './privacy-policy/privacy-policy';
 
 export const routes: Routes = [
   { path: '', component: LandingPage },
   { path: 'login', component: Login },
   { path: 'signup', component: Signup },
   { path: 'voting-poll', component: VotingPoll },
+  { path: 'terms-of-service', component: TermsOfService },
+  { path: 'privacy-policy', component: PrivacyPolicy },
   { path: '**', redirectTo: '' }
 ];
