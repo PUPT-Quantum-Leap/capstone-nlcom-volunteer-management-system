58ef9f09cd8b0d6afb087cdd49f9c8201994746b
ynion-mabeamae <mabeamaeynion@gmail.com>
2026-02-28 09:59:34 +0800

    feat: add signup-form route to application routing

--- servetrack-frontend/src/app/app.routes.ts
+++ servetrack-frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LandingPage } from './landing-page/landing-page';
 import { VotingPoll } from './voting-poll/voting-poll';
 import { Login } from './auth/login/login';
 import { Signup } from './auth/signup/signup';
+import { SignupForm } from './auth/signup-form/signup-form';
 
 export const routes: Routes = [
   { path: '', component: LandingPage },
   { path: 'login', component: Login },
   { path: 'signup', component: Signup },
+  { path: 'signup-form', component: SignupForm },
   { path: 'voting-poll', component: VotingPoll },
   {
     path: 'terms-of-service',
