212bd190449ba241ed03d8273f880ad510e6b378
ynion-mabeamae <mabeamaeynion@gmail.com>
2026-03-01 13:34:09 +0800

    feat: add NotFound component with HTML, SCSS, and unit tests

---
+++ servetrack-frontend/src/app/not-found/not-found.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-not-found',
+  imports: [],
+  templateUrl: './not-found.html',
+  styleUrl: './not-found.scss',
+})
+export class NotFound {
+  constructor(private router: Router) {}
+
+  goHome(): void {
+    this.router.navigate(['/']);
+  }
+
+  goBack(): void {
+    window.history.back();
+  }
+}
