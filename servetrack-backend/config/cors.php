afcad24bc8f77a8164246d1eac3fb2917d629c22
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-03-03 00:26:23 +0800

    feat: extend pathing

--- servetrack-backend/config/cors.php
+++ servetrack-backend/config/cors.php
@@ -19,7 +19,12 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:4200', 'http://127.0.0.1:4200'],
+    'allowed_origins' => [
+        'http://localhost:4200',
+        'http://localhost:4201',
+        'http://127.0.0.1:4200',
+        'http://127.0.0.1:4201',
+    ],
 
     'allowed_origins_patterns' => [],
 
