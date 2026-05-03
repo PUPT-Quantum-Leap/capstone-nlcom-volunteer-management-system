239435c06c0d3824b2846bd57c80a980f858e132
mejares-jamesmichael <jamesmichaelmejares@gmail.com>
2026-05-04 02:29:36 +0800

    feat(security): implement global input sanitization middleware to strip HTML tags from string inputs (XSS protection)

---
+++ servetrack-backend/app/Http/Middleware/StripTags.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\TransformsRequest;
+
+class StripTags extends TransformsRequest
+{
+    /**
+     * The attributes that should not be stripped of tags.
+     *
+     * @var array<int, string>
+     */
+    protected $except = [
+        'password',
+        'password_confirmation',
+        'currentPassword',
+        'newPassword',
+        'newPassword_confirmation',
+        'confirmPassword',
+    ];
+
+    /**
+     * Transform the given value.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return mixed
+     */
+    protected function transform($key, $value)
+    {
+        if (in_array($key, $this->except, true)) {
+            return $value;
+        }
+
+        return is_string($value) ? strip_tags($value) : $value;
+    }
+}
