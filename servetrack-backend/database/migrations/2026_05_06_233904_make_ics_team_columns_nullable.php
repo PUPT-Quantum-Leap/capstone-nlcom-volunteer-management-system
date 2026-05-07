d5ba36e297171dc2135c28d9430c838c4a144e7c
ynion-mabeamae <mabeamaeynion@gmail.com>
2026-05-07 11:44:35 +0800

    feat(migration): create migration to make ics_team columns nullable

---
+++ servetrack-backend/database/migrations/2026_05_06_233904_make_ics_team_columns_nullable.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('ics_team', function (Blueprint $table) {
+            $table->unsignedBigInteger('ics_id')->nullable()->change();
+            $table->unsignedBigInteger('team_id')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('ics_team', function (Blueprint $table) {
+            $table->unsignedBigInteger('ics_id')->nullable(false)->change();
+            $table->unsignedBigInteger('team_id')->nullable(false)->change();
+        });
+    }
+};
